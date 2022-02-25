0b6dd71fc9d55aad9de373c3c54a823a58b5c4df
Andrés Cañón <mario.canon@datatools.com.co>
2022-02-25 16:50:01 -0500

    Initial commit

---
+++ src/test/java/com/ms/app/coursesmicroservice/CoursesMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.app.coursesmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
