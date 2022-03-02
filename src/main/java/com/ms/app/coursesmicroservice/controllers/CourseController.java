11f51ebd30cf3c619b125c23dceec2d2ab9fdb8a
Andrés Cañón <mario.canon@datatools.com.co>
2022-03-02 11:31:16 -0500

    CourseController - add-students and remove-student from course.

--- src/main/java/com/ms/app/coursesmicroservice/controllers/CourseController.java
+++ src/main/java/com/ms/app/coursesmicroservice/controllers/CourseController.java
@@ -1,5 +1,6 @@
 package com.ms.app.coursesmicroservice.controllers;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ms.app.commons.controllers.CommonController;
+import com.ms.app.commonsstudents.models.entity.Student;
 import com.ms.app.coursesmicroservice.models.entity.Course;
 import com.ms.app.coursesmicroservice.services.CourseService;
 
@@ -24,6 +26,30 @@ public class CourseController extends CommonController<Course, CourseService> {
 		}
 		Course courseToDataBase = optional.get();
 		courseToDataBase.setName(course.getName());
+		return ResponseEntity.status(HttpStatus.CREATED).body(this._service.save(courseToDataBase));
+	}
+
+	@PutMapping("/{id}/add-students")
+	public ResponseEntity<?> addStudents(@RequestBody List<Student> students, @PathVariable Long id) {
+		Optional<Course> optional = this._service.findById(id);
+		if (!optional.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		Course courseToDataBase = optional.get();
+		students.forEach(s -> {
+			courseToDataBase.addStudent(s);
+		});
+		return ResponseEntity.status(HttpStatus.CREATED).body(this._service.save(courseToDataBase));
+	}
+
+	@PutMapping("/{id}/remove-student")
+	public ResponseEntity<?> removeStudentFromCourse(@RequestBody Student student, @PathVariable Long id) {
+		Optional<Course> optional = this._service.findById(id);
+		if (!optional.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		Course courseToDataBase = optional.get();
+		courseToDataBase.removeStudent(student);
 		return ResponseEntity.status(HttpStatus.CREATED).body(this._service.save(courseToDataBase));
 	}
 
